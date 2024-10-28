db50e2c2e7b546eab63a029f5b3fc80113c30f73
RUSHIKESHCHEEDALLA <cheedallarushikesh@gmail.com>
2024-10-28 23:19:46 +0530

    eigth-commit

---
+++ Servers/App.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const alienRouter = require('./Routes/aliens')
+
+
+const url = 'mongodb://127.0.0.1:27017/CBIT';
+const app = express()
+mongoose.connect(url)
+const con = mongoose.connection
+
+
+con.on('open', () =>
+{
+console.log('connected...')
+})
+
+app.use(express.json())
+
+app.use('/alien',alienRouter)
+app.listen(6000, () =>
+{
+console.log('Server started')
+})
